ff4b1b1f2f0a989d14f234e68390c8401afcffac
altabeh <proof.beh@gmail.com>
2021-01-06 22:13:34 -0500

    fixed Google scholar url

--- tools/gcl/gcl.py
+++ tools/gcl/gcl.py
@@ -1,7 +1,7 @@
 
 """
 This module provides a parser for Google case law
-pages available at https://www.scholar.google.com/.
+pages available at https://scholar.google.com/.
 
 The offered features include downloading, scraping, parsing, serializing
 and tagging important data such as bluebook citations, judge names, courts,
