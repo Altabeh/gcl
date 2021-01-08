a8a86e93a0d3116c5b04d308370184363a616c38
altabeh <proof.beh@gmail.com>
2021-01-08 01:09:08 -0500

    added new functions

--- tools/utils.py
+++ tools/utils.py
@@ -1,58 +1,111 @@
 import re
 import unicodedata
-from pathlib import Path
-from subprocess import PIPE, CalledProcessError, Popen, check_output
 import urllib
+from concurrent.futures import ProcessPoolExecutor as future_pool
+from os import cpu_count
+from pathlib import Path
 
-__all__ = ['pdf_to_text', 'get_page_count',
-           'sort_int', 'deaccent', 'normalize',
-           'closest_value', 'hyphen_to_numbers',
-           'remove_repeated', 'validate_url']
+from tqdm import tqdm
+
+__all__ = ['create_dir', 'multiprocess', 'regex', 'sort_int', 'deaccent', 'normalize',
+           'closest_value', 'hyphen_to_numbers', 'remove_repeated', 'validate_url']
 
 
 DOMAIN_FORMAT = re.compile(
-    r'(?:^(\w{1,255}):(.{1,255})@|^)' # http basic authentication [optional]
-    r'(?:(?:(?=\S{0,253}(?:$|:))' # check full domain length to be less than or equal to 253 (starting after http basic auth, stopping before port)
-    r'((?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+' # check for at least one subdomain (maximum length per subdomain: 63 characters), dashes in between allowed
-    r'(?:[a-z0-9]{1,63})))' # check for top level domain, no dashes allowed
-    r'|localhost)' # accept also 'localhost' only
-    r'(:\d{1,5})?', # port [optional]
+    r'(?:^(\w{1,255}):(.{1,255})@|^)'  # http basic authentication [optional]
+    # check full domain length to be less than or equal to 253 (starting after http basic auth, stopping before port)
+    r'(?:(?:(?=\S{0,253}(?:$|:))'
+    # check for at least one subdomain (maximum length per subdomain: 63 characters), dashes in between allowed
+    r'((?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+'
+    r'(?:[a-z0-9]{1,63})))'  # check for top level domain, no dashes allowed
+    r'|localhost)'  # accept also 'localhost' only
+    r'(:\d{1,5})?',  # port [optional]
     re.IGNORECASE
 )
 SCHEME_FORMAT = re.compile(
-    r'^(http|hxxp|ftp|fxp)s?$', # scheme: http(s) or ftp(s)
+    r'^(http|hxxp|ftp|fxp)s?$',  # scheme: http(s) or ftp(s)
     re.IGNORECASE
 )
 
-def pdf_to_text(pdf_path, target_dir):
+
+def regex(item,
+          patterns=None,
+          sub=True,
+          flags=None,
+          start=0,
+          end=None):
     """
-    Convert pdf at `pdf_path` to a txt file in `target_dir` using xpdf.
+    Apply a regex rule to find/substitute a textual pattern in the text.
+
+    Args
+    ----
+    :param item: ---> list or str: list of strings/string to apply regex to.
+    :param patterns: ---> list of tuples: regex patterns.
+    :param sub: ---> bool: switch between re.sub/re.search.
+    :param flags: ---> same as `re` flags. Defaults to `None` or `0`.
+    :param start: ---> int: start index of the input list from which applying regex begins.
+    :param end: ---> int: end index of the input list up to which applying regex continues.
     """
-    file_name = Path(pdf_path).stem
-    command = ["pdftotext", "-layout", pdf_path,
-               str(Path(target_dir) / f'{file_name}.txt')]
-    proc = Popen(
-        command, stdout=PIPE, stderr=PIPE)
-    proc.wait()
-    (stdout, stderr) = proc.communicate()
-    if proc.returncode:
-        return stderr
-    return ''
+    if not patterns:
+        raise Exception("Please enter a valid pattern e.g. [(r'\n', '')]")
+
+    if not flags:
+        flags = 0
+
+    if item:
+        for pattern, val in patterns:
+            if isinstance(item, list):
+                if isinstance(item[0], list):
+                    if sub:
+                        item = [list(map(lambda x: re.sub(
+                            pattern, val, x, flags=flags), group[start:end])) for group in item]
+                    else:
+                        item = [list(map(lambda x: re.findall(
+                            pattern, x, flags=flags), group[start:end])) for group in item]
+
+                if isinstance(item[0], str):
+                    if sub:
+                        item = [re.sub(pattern, val, el, flags=flags)
+                                for el in item[start:end]]
+                    else:
+                        item = [re.findall(pattern, el, flags=flags)
+                                for el in item[start:end]]
+
+            elif isinstance(item, str):
+                if sub:
+                    item = re.sub(pattern, val, item, flags=flags)
+                else:
+                    item = re.findall(pattern, item, flags=flags)
+            else:
+                continue
+    return item
 
 
-def get_page_count(pdf_path):
+def create_dir(path):
     """
-    Use xpdf's pdfinfo to extract the number of pages in a pdf file.
+    Create a directory under `path`.
     """
-    try:
-        output = check_output(["pdfinfo", pdf_path]).decode()
-        pages_line = [line for line in output.splitlines()
-                      if "Pages:" in line][0]
-        num_pages = int(pages_line.split(":")[1])
-        return num_pages
+    if isinstance(path, str):
+        path = Path(path)
+    path.mkdir(parents=True, exist_ok=True)
+    return path
 
-    except (CalledProcessError, UnicodeDecodeError):
-        return 0
+
+def multiprocess(func,
+                 files,
+                 yield_results=False,
+                 cpus=cpu_count()):
+    """
+    Wrap a function `func` in a multiprocessing block good for simultaneous I/O operations
+    involving multiple number of `files`. 
+    Set `yield_results` to True if intend to yield results back to the caller.
+    """
+    with future_pool(max_workers=cpus) as p:
+        for _ in tqdm(p.map(func, files), total=len(files)):
+            if not yield_results:
+                pass
+            else:
+                yield _
 
 
 def sort_int(string):
@@ -102,7 +155,8 @@ def hyphen_to_numbers(string):
     >>> hyphen_to_numbers('3-5')
     '3 4 5'
     """
-    string_lst = list(map(lambda x : re.sub(r'^-|-$', '', x), string.split(' ')))
+    string_lst = list(map(lambda x: re.sub(
+        r'^-|-$', '', x), string.split(' ')))
     final_list = []
 
     for x in string_lst:
@@ -113,6 +167,7 @@ def hyphen_to_numbers(string):
         else:
             final_list += [x.replace('-', '')]
     return ' '.join(final_list)
+
 
 def remove_repeated(l):
     """
@@ -128,7 +183,8 @@ def validate_url(url: str):
         raise Exception('No URL specified')
 
     if len(url) > 2048:
-        raise Exception('URL exceeds its maximum length of 2048 characters (given length={len(url)})')
+        raise Exception(
+            'URL exceeds its maximum length of 2048 characters (given length={len(url)})')
 
     result = urllib.parse.urlparse(url)
     scheme = result.scheme
@@ -138,7 +194,8 @@ def validate_url(url: str):
         raise Exception('No URL scheme specified')
 
     if not re.fullmatch(SCHEME_FORMAT, scheme):
-        raise Exception(f'URL scheme must either be http(s) or ftp(s) (given scheme={scheme})')
+        raise Exception(
+            f'URL scheme must either be http(s) or ftp(s) (given scheme={scheme})')
 
     if not domain:
         raise Exception('No URL domain specified')
